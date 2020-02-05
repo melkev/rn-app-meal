c0ea60c00bb642215881ce11fdfb8696cd3974e1
melkev <51228075+melkev@users.noreply.github.com>
2020-02-05 19:12:21 +0100

    update navigationOption from CategoryMealsScreen

--- screen/CategoryMealsScreen.js
+++ screen/CategoryMealsScreen.js
@@ -1,9 +1,10 @@
 import React from "react";
-import { StyleSheet, View, Text, Button } from "react-native";
+import { StyleSheet, View, Text, Button, Platform } from "react-native";
 
-//
+//data
 import { CATEGORIES } from "../data/dummy-data";
-
+//color
+import Colors from "../constants/Color";
 const CategoriesMealsScreen = props => {
   // recup l'id du title sur lequel on a clicker dans la page categoriesScreen est laffiche dans le text
   const catId = props.navigation.getParam("categoryId");
@@ -34,8 +35,12 @@ CategoriesMealsScreen.navigationOptions = navigationData => {
   const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
 
   return {
-    headerTitle: selectedCategory.title
-  }
+    headerTitle: selectedCategory.title,
+    headerStyle: {
+      backgroundColor: Platform.OS === "android" ? Colors.primaryColor : "white"
+    },
+    headerTintColor: Platform.OS === "android" ? "white" : "black"
+  };
 };
 
 // css
