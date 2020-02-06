912ddc764ec0d560b0452888c39972083029e7f4
melkev <51228075+melkev@users.noreply.github.com>
2020-02-06 13:53:19 +0100

    passing data to the MealDetailScreen

--- components/MealItem.js
+++ components/MealItem.js
@@ -18,13 +18,14 @@ const MealItem = props => {
               style={styles.bgImage}
             >
               <View style={styles.titleContainer}>
-              <Text style={styles.title} numberOfLines={1} >{props.title}</Text>
-
+                <Text style={styles.title} numberOfLines={1}>
+                  {props.title}
+                </Text>
               </View>
             </ImageBackground>
           </View>
           <View style={{ ...styles.mealRow, ...styles.mealDetail }}>
-            <Text> {props.duration}m </Text>
+            <Text> {props.duration} Min </Text>
             <Text> {props.complexity.toUpperCase()} </Text>
             <Text> {props.affordability.toUpperCase()} </Text>
           </View>
@@ -40,12 +41,12 @@ const styles = StyleSheet.create({
     width: "100%",
     backgroundColor: "#f5f5f5",
     borderRadius: 10,
-    overflow: 'hidden'
+    overflow: "hidden"
   },
   bgImage: {
     width: "100%",
     height: "100%",
-    justifyContent: 'flex-end'
+    justifyContent: "flex-end"
   },
   mealRow: {
     flexDirection: "row"
@@ -56,20 +57,20 @@ const styles = StyleSheet.create({
   mealDetail: {
     paddingHorizontal: 10,
     justifyContent: "space-between",
-    alignItems: 'center',
-    height:'15%'
+    alignItems: "center",
+    height: "15%"
   },
-  titleContainer:{
-    backgroundColor: 'rgba(0,0,0,0.5)',
+  titleContainer: {
+    backgroundColor: "rgba(0,0,0,0.5)",
     paddingVertical: 5,
-    paddingHorizontal: 12,
+    paddingHorizontal: 12
   },
-  title:{
-    fontFamily: 'open-sans-bold',
+  title: {
+    fontFamily: "open-sans-bold",
     fontSize: 20,
-    color: 'white',
-   
-    textAlign: 'center'
+    color: "white",
+
+    textAlign: "center"
   }
 });
 
